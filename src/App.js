8c268668cca2589ed7dc264faf6b97f818e989cb
MokiMo <bannedjobs@gmail.com>
2018-07-11 23:11:12 +0200

    Cleanup App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import AppRouter from "./router/AppRouter";
-class App extends Component {
-  render() {
-    const spinner = document.getElementById("spinner");
-
-    if (spinner && !spinner.hasAttribute("hidden")) {
-      spinner.setAttribute("hidden", "true");
-    }
-    return <AppRouter />;
-  }
-}
-
+const App = () => <AppRouter />;
 export default App;
