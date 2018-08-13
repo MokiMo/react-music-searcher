8ab36e40814992349e961122cbcec2d579bd7f25
MokiMo <bannedjobs@gmail.com>
2018-08-13 16:12:59 +0200

    API-Snapshot tests added. //bugfix QoL if LastFM Api changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
+import axios from 'axios';
 import App from './App';
+import API_KEY from './containers/MusicSearch/API_KEY';
 import { Login } from './containers/Login/LoginForm';
 import Dashboard from './containers/Dashboard/Dashboard';
 import Footer from './containers/Footer/Footer';
@@ -10,6 +13,36 @@ import AuthReducer, { initState as AuthInitState } from './redux/auth/reducer';
 import AuthActions from './redux/auth/actions';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+// If the LastFM-search-API has breaking changes, use the snapshot to troubleshoot
+describe('API', () => {
+  it('API-Request returns data matching Snapshot (TRACKS)', async () => {
+    const response = await axios
+      .get(
+        `https://ws.audioscrobbler.com/2.0/?method=track.search&track=Jack&api_key=${API_KEY}&format=json&limit=30`
+      )
+      .then(res => res.data.results.trackmatches.track);
+    expect(response).toMatchSnapshot();
+  });
+
+  it('API-Request returns data matching Snapshot (ALBUMS)', async () => {
+    const response = await axios
+      .get(
+        `https://ws.audioscrobbler.com/2.0/?method=album.search&album=Jack&api_key=${API_KEY}&format=json&limit=30`
+      )
+      .then(res => res.data.results.albummatches.album);
+    expect(response).toMatchSnapshot();
+  });
+
+  it('API-Request returns data matching Snapshot (ARTIST)', async () => {
+    const response = await axios
+      .get(
+        `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=Jack&api_key=${API_KEY}&format=json&limit=30`
+      )
+      .then(res => res.data.results.artistmatches.artist);
+    expect(response).toMatchSnapshot();
+  });
+});
 
 describe('App', () => {
   it('shallow renders App without crashing', () => {
