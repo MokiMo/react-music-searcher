4438caa1b5b59f46ff45dcac3b8a7b0778ba042a
MokiMo <bannedjobs@gmail.com>
2019-03-30 20:24:04 +0100

    refactor: move config and create secrets

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import MusicNote from '@material-ui/icons/MusicNote';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import StarIcon from '@material-ui/icons/Star';
-import MailIcon from '@material-ui/icons/Mail';
-import DynamicImport from './router/DynamicImport';
-import Loading from './containers/Loading/Loading';
+import DynamicImport from '../router/DynamicImport';
+import Loading from '../containers/Loading/Loading';
 
 const dev = true;
 export const authenticated = !!dev;
@@ -15,14 +13,14 @@ export const API_KEY = process.env.LAST_FM_API_KEY || 'local_test_key';
 // 2) add route to the route-array, path:component
 // 3) add to sidebar
 export const NotFoundPage = props => (
-  <DynamicImport load={() => import('./containers/NotFound/NotFoundPage')}>
+  <DynamicImport load={() => import('../containers/NotFound/NotFoundPage')}>
     {Component => (Component === null ? <Loading /> : <Component {...props} />)}
   </DynamicImport>
 );
 
 const LayoutSearchMusic = props => (
   <DynamicImport
-    load={() => import('./containers/MusicSearch/LayoutSearchMusic')}
+    load={() => import('../containers/MusicSearch/LayoutSearchMusic')}
   >
     {Component => (Component === null ? <Loading /> : <Component {...props} />)}
   </DynamicImport>
@@ -30,7 +28,7 @@ const LayoutSearchMusic = props => (
 
 const FavoriteMusic = props => (
   <DynamicImport
-    load={() => import('./containers/FavoriteMusic/FavoriteMusic')}
+    load={() => import('../containers/FavoriteMusic/FavoriteMusic')}
   >
     {Component => (Component === null ? <Loading /> : <Component {...props} />)}
   </DynamicImport>
@@ -44,10 +42,8 @@ export const routes = [
 
 export const SidebarTop = [
   { text: 'Search Music', icon: <MusicNote />, route: 'searchmusic' },
-  { text: 'Favorite Music', icon: <StarIcon />, route: 'favoritemusic' },
 ];
 
 export const SidebarBot = [
-  { text: 'Profile*', icon: <MailIcon />, route: 'all_emails' },
-  { text: 'About*', icon: <InboxIcon />, route: 'about' },
+  { text: 'Favorite Music', icon: <StarIcon />, route: 'favoritemusic' },
 ];
