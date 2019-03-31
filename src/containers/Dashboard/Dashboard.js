8e42c479c306b1043385ae252ce9872e969118f5
mokimo <bannedjobs@gmail.com>
2019-04-01 00:19:40 +0200

    fix: add redirect on the dashboard

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import { NavLink } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 
@@ -13,40 +11,13 @@ const styles = theme => ({
   },
 });
 
-const Dashboard = ({ classes }) => (
-  <Paper className={classes.Paper}>
-    <Typography
-      variant="display1"
-      align="center"
-      className={classes.Paper}
-      gutterBottom
-    >
-      Dashboard.
-      <br />
-      ...
-      <br />
-      ...
-      <br />
-      ...
-      <br />
-      Business Content
-      <br />
-      ...
-      <br />
-      ...
-      <br />
-      ...
-    </Typography>
-    <Typography variant="display1" align="center" className={classes.Paper}>
-      Demo Features
-      <br />- <NavLink to="/searchmusic">Search Music</NavLink>
-      <br />- <NavLink to="/favoritemusic">Favorite Music</NavLink>
-    </Typography>
-  </Paper>
-);
+const Dashboard = props => {
+  props.history.push('/searchmusic');
+  return <div />;
+};
 
 Dashboard.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(Dashboard);
+export default withRouter(withStyles(styles)(Dashboard));
